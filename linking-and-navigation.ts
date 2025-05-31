ae87c04a3ae1ceed2cab4862417b210773e4ec94
jeevan42 <g1kumar05092002@gmail.com>
2025-05-31 12:02:24 +0530

    added linking-and-navigation.ts file theory based

---
+++ linking-and-navigation.ts
@@ -0,0 +1,23 @@
+// ✅ LINKING AND NAVIGATION IN NEXT.JS 15 (App Router)
+
+// 1️⃣ Client-Side Navigation using <Link>
+/*
+Next.js provides a built-in <Link> component from 'next/link' 
+that enables client-side transitions between routes without full reload.
+*/
+
+// ---------------CODE-START--------------- //
+// import Link from "next/link";
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//       <Link href="/about">
+//         <button>Go to About Page</button>
+//       </Link>
+//     </div>
+//   );
+// }
+
+// ---------------CODE-END--------------- //
